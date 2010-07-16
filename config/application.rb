1f0e5afcbeeb4e78dc9f7922c94ac6f6e99de5fb
Raving Genius <rg+code@ravinggenius.com>
2010-07-16 00:43:08 -0400

    Configured generators

--- config/application.rb
+++ config/application.rb
@@ -36,11 +36,11 @@ module RavinggeniusOrms
     # config.i18n.default_locale = :de
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
-    # config.generators do |g|
-    #   g.orm             :active_record
-    #   g.template_engine :erb
-    #   g.test_framework  :test_unit, :fixture => true
-    # end
+    config.generators do |g|
+      g.orm             :datamapper
+      g.template_engine :haml
+      g.test_framework  :shoulda, :fixture => true
+    end
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
